6c7cb5e872919de267d40611c395c5f10528e31d
SparkZHarrY <66181850+SparkZHarrY@users.noreply.github.com>
2024-09-09 19:05:16 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -124,7 +124,15 @@ async def start_command(client: Client, message: Message):
 
     
 
-
+@Bot.on_message(filters.command("premium"))
+async def premium(client, message):
+    text = "✨ Unlock AD's Free Content By Joining Our Premium Channel! Click The Button Below To Join."
+    keyboard = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton("Join Premium Channel", url="https://t.me/your_channel_link")]
+        ]
+    )
+    await message.reply_text(text, reply_markup=keyboard)
 
     
     
