dcd570e4207e30be6aff33b8867001e684284817
SparkZHarrY <66181850+SparkZHarrY@users.noreply.github.com>
2024-09-11 15:18:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -247,4 +247,4 @@ async def delete_files(messages, client, k):
         except Exception as e:
             print(f"The attempt to delete the media {msg.id} was unsuccessful: {e}")
     # await client.send_message(messages[0].chat.id, "Your Video / File Is Successfully Deleted ✅")
-    await k.edit_text("Your File Is Successfully Deleted ✅")
+    await k.edit_text("The Previous File Is Successfully Deleted ✅ Re-Download it From @actressuniverseofficial")
